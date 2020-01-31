236e48fa5a7cd9a9d43dd1591937f3601844537d
Scala Steward <helber.sissa@mail.escuelaing.edu.co>
2020-01-31 18:15:11 +0000

    Update cats-effect to 2.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Versions {
   lazy val cats        = "2.1.0"
-  lazy val catsEffects = "2.0.0"
+  lazy val catsEffects = "2.1.0"
   lazy val miniTest    = "2.7.0"
   lazy val mouse       = "0.23"
   lazy val scalaCheck  = "1.14.3"
