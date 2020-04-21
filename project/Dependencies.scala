8a83d4fcae379f26e6cc88b3fbc3d8da6e01f692
Scala Steward <helber.sissa@mail.escuelaing.edu.co>
2020-04-21 18:06:01 +0000

    Update mouse to 0.25

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Versions {
   lazy val cats        = "2.1.1"
   lazy val catsEffects = "2.1.2"
   lazy val miniTest    = "2.7.0"
-  lazy val mouse       = "0.23"
+  lazy val mouse       = "0.25"
   lazy val scalaCheck  = "1.14.3"
   lazy val doobie      = "0.8.8"
   lazy val squants     = "1.6.0"
