fc7a2b558e4796cd8fd6f3fd5f2f9fcbdc74b085
Giovanny Sissa <guiovannys@hotmail.es>
2020-05-01 20:36:21 -0500

    fix deployment file

--- app/scripts/run.sh
+++ app/scripts/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 export NAMESPACE=dev-giovannysissa
-export GRPC_IMAGE=${REGISTRY_ADDRESS}"ref-arch-grpc":$(sbt version | tail -1 | awk '{print $2}')
+export GRPC_IMAGE=${REGISTRY_ADDRESS}"/ref-arch-grpc":$(sbt version | tail -1 | awk '{print $2}')
 cat app/grpc-deployment.yml | envsubst | kubectl -n $NAMESPACE apply -f -
